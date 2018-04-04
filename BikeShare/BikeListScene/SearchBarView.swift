72a5fb070fd8e553b1bfec83ba8becd1a8b19656
Slone Daniel <daniel.slone@suncorp.com.au>
2018-04-04 11:16:38 +1000

    [Mapbox]
    * SearchBarView added

--- BikeShare/BikeListScene/SearchBarView.swift
+++ BikeShare/BikeListScene/SearchBarView.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
-class SearchBarView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+protocol SearchBarDelegate {
+    func textDidChange(searchText: String)
+    func isSearchBar(active: Bool)
+}
+class SearchBarView: UIView, UISearchBarDelegate {
+    
+    let searchBar: UISearchBar
+    var searchBarDelegate: SearchBarDelegate?
+    //var onTextChange: ((SearchBar) -> Void)?
+    
+    init(customSearchBar: UISearchBar) {
+        searchBar = customSearchBar
+        super.init(frame: CGRect.zero)
+        
+        searchBar.delegate = self
+        searchBar.placeholder = "Search"
+        searchBar.barTintColor = UIColor.white
+        searchBar.searchBarStyle = .minimal
+        searchBar.returnKeyType = .done
+        addSubview(searchBar)
     }
-    */
-
+    
+    override convenience init(frame: CGRect) {
+        self.init(customSearchBar: UISearchBar())
+        self.frame = frame
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        searchBar.frame = bounds
+    }
+    
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        print("should end ")
+        searchBarDelegate?.isSearchBar(active: false)
+        return true
+    }
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        print("should begin")
+        searchBarDelegate?.isSearchBar(active: true)
+        return true
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("cancel")
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print("did end")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchBarDelegate?.textDidChange(searchText: searchText)
+    }
 }
