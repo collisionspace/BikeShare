5c81b46ca4ec033082efa2687f51f4b4561b48f4
Slone Daniel <daniel.slone@suncorp.com.au>
2018-04-05 16:08:25 +1000

    [MAPBOX]
    * Final update

--- BikeShare/LocationServices.swift
+++ BikeShare/LocationServices.swift
@@ -27,7 +27,6 @@ class LocationServices: NSObject {
     
     weak var delegate: LocationChangeDelegate?
     weak var locationServicesEnabledDelegate:LocationServicesEnabledDelegate?
-    var location:CLLocation?
     
     private override init() {
         super.init()
@@ -66,7 +65,6 @@ extension LocationServices: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
-            self.location = location
             delegate?.locationChange(location: location)
         }
     }
